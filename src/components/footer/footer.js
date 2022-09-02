d6e14b2749d662d7c128510bbe54ff78d2d816da
Owen Buckley <owenbuckley13@gmail.com>
2022-09-01 21:21:20 -0400

    clean up deps

---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+export default class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer><h6>Tuesdays Tunes &copy${new Date().getFullYear()}</h6></footer>
+    `
+  }
+}
+
+customElements.define('tt-footer', Footer);
