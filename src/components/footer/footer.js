a82a382e0d9b916dd5b152b6115614f3fb11fc56
Owen Buckley <owen@thegreenhouse.io>
2022-10-05 22:19:12 -0400

    Feature/issue 13 theme and styleguide (#27)
    * initial prototyping with Tailwind
    
    * downgrade tailwind v2 for Storybook compat
    
    * enable tailwind JiT mode for development
    
    * full storybook based documentation and font families

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,9 @@
 export default class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <footer><h6 id="copyright">Tuesday's Tunes &copy${new Date().getFullYear()}</h6></footer>
+      <footer>
+        <h6 id="copyright" class="text-3xl font-bold underline">Tuesday's Tunes &copy${new Date().getFullYear()}</h6>
+      </footer>
     `;
   }
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{html,js}'],
+  mode: 'jit',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
