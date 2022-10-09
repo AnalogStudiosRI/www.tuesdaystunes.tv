e5f1d2fe02763604c33e58224ceac7398a2f47fb
Owen Buckley <owen@thegreenhouse.io>
2022-10-09 16:42:42 -0400

    handle multiple months, multiple events, and out order sorting

--- src/components/upcoming-events/upcoming-events.js
+++ src/components/upcoming-events/upcoming-events.js
@@ -15,51 +15,72 @@ const MONTH_INDEX_MAPPER = [
 
 export default class UpcomingEvents extends HTMLElement {
   connectedCallback() {
+    const eventsByMonth = {};
     const events = (JSON.parse(this.getAttribute('events')) || [])
-      .filter(event => event.timestamp >= Date.now());
+      .filter(event => event.timestamp >= Date.now()) // filter out old events
+      .sort((a, b) => a.timestamp < b.timestamp ? -1 : 1); // sort newest to latest
 
-    console.debug({ events });
+    // group events by month
+    events.forEach((event) => {
+      const time = new Date(event.timestamp);
+      const month = time.getMonth();
+      const monthKey = MONTH_INDEX_MAPPER[month];
+
+      if (!eventsByMonth[monthKey]) {
+        eventsByMonth[monthKey] = [];
+      }
+
+      eventsByMonth[monthKey].push(event);
+    });
 
     /* eslint-disable indent */
     this.innerHTML = `
       <div>
-      ${events.map((event) => {
-        const { link, timestamp, title } = event;
-        const time = new Date(timestamp);
-        const month = time.getMonth();
-        const date = time.getDate();
-        const hour = time.getHours() - 12; // here we assume an 8pm (e.g. afternoon) start time
-
-        return `
-          <div>
-            <h2 
-              style="background-color:var(--color-secondary);color:#efefef"
-              class="text-center p-2 mb-2 text-3xl font-bold"
-            >
-              ${MONTH_INDEX_MAPPER[month]}
-            </h2>
-            <h3 
-              style="color:#efefef"
-              class="text-center"
-            >
-              <a 
-                href="${link}"
-                alt="${title}"
-              >
-                <span
-                  class="inline-block w-8 text-center"
-                  style="background-color:var(--color-accent);"
+        ${
+          Object.keys(eventsByMonth).map((month) => {
+            return `
+              <div class="mb-6">
+                <h2 
+                  style="background-color:var(--color-secondary);color:#efefef"
+                  class="text-center p-2 mb-4 text-3xl font-bold"
                 >
-                  ${date}
-                </span>
-                <span
-                  style="color:var(--color-secondary);"
-                >${title} @ ${hour}pm</span>
-              </a>
-            </h3>
-          </div>
-        `;
-      })}
+                  ${month}
+                </h2>
+
+                ${eventsByMonth[month].map((event) => {
+                  const { link, timestamp, title } = event;
+                  const time = new Date(timestamp);
+                  const date = time.getDate();
+                  const hour = time.getHours() - 12; // here we assume an 8pm (e.g. afternoon) start time
+          
+                  return `
+                    <div>
+                      <h3 
+                        style="color:#efefef"
+                        class="text-center"
+                      >
+                        <a 
+                          href="${link}"
+                          alt="${title}"
+                        >
+                          <span
+                            class="inline-block w-8 text-center"
+                            style="background-color:var(--color-accent);"
+                          >
+                            ${date}
+                          </span>
+                          <span
+                            style="color:var(--color-secondary);"
+                          >${title} @ ${hour}pm</span>
+                        </a>
+                      </h3>
+                    </div>
+                  `;
+                })}
+              </div>
+            `;
+          }).join('')
+        }      
       </div>
     `;
   }
--- src/components/upcoming-events/upcoming-events.stories.js
+++ src/components/upcoming-events/upcoming-events.stories.js
@@ -2,18 +2,21 @@ import '../../styles/theme.css';
 import './upcoming-events.js';
 
 const SINGLE_EVENT = [{
-  title: 'Tuesdays Tunes Season Premier!',
+  title: 'Tuesdays Tunes Season 3 - Premier!',
   timestamp: Date.now() + 300000,
   link: 'http://www.facebook.com/'
 }];
 
+// includes testing for multiple months, events list "overflow", and out of order events
 const MULTIPLE_EVENTS = [{
-  title: 'Tuesdays Tunes Season Premier!',
-  timestamp: Date.now() + 300000,
+  title: 'Tuesdays Tunes Season 3 - Episode 1',
+  timestamp: SINGLE_EVENT[0].timestamp + (86400000 * 15),
   link: 'http://www.facebook.com/'
 }, {
-  title: 'Tuesdays Tunes Season Premier!!!!!',
-  timestamp: Date.now() + 30000000000,
+  ...SINGLE_EVENT[0]
+}, {
+  title: 'Tuesdays Tunes Season 3 - Episode 2',
+  timestamp: SINGLE_EVENT[0].timestamp + (86400000 * 30),
   link: 'http://www.facebook.com/'
 }];
 
