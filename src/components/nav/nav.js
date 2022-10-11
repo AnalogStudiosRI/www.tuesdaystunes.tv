61cf4dfbcd0585505dd822b037f1fc5e17f96b42
aholtzman <adam.holtzman@gmail.com>
2022-10-11 11:56:38 -0400

    pr feednabck: lists && apply rebased styles

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -27,25 +27,30 @@ export default class Nav extends HTMLElement {
     const { navLinks } = this;
 
     this.innerHTML = `
-      <nav class="flex justify-center p-4">
+      <nav>
+      <ul class="flex justify-center p-4 font-title text-2xl text-black-tt">
         ${
           navLinks.map((link, index) => {
             const { name, url } = link;
             const isLast = index + 1 === navLinks.length;
-            const activePath = window.location.path === `/${name}`;
+            const path = window !== undefined ? window.location.pathname : null;
+            const activePath = path === `/${name}`;
 
             return `
-              <a
-                class="nav-link text-2xl text-${activePath ? 'red' : 'black'}"
-                href="${url}"
-                title="Visit our ${name} page"
-              >
-              ${name}
-              <span class="pr-${ isLast ? 0 : 2 }">${ isLast ? '' : '|' }</span>
-              </a>
+              <li>
+                <a
+                  class="nav-link ${activePath ? 'text-orange-tt' : 'text-black-tt'} hover:text-gray-tt"
+                  href="${url}"
+                  title="Visit our ${name} page"
+                >
+                ${name}
+                </a>
+                <span style="margin: 0 ${isLast ? 0 : '1rem' };">${ isLast ? '' : '|'}</span>
+              </li>
             `;
           }).join('')
         }
+        </ul>
       </nav>
     `;
   }
