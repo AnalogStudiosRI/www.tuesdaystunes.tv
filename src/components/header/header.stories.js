1a043fc07567536a6801ce889633ab4be7812ad4
Owen Buckley <owen@thegreenhouse.io>
2022-10-20 09:10:31 -0400

    file and Storybook naming consistency

--- src/components/header/header.stories.js
+++ src/components/header/header.stories.js
@@ -8,4 +8,4 @@ export default {
 // More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
 const Template = () => '<tt-header></tt-header>';
 
-export const Header = Template.bind({});
+export const Primary = Template.bind({});
