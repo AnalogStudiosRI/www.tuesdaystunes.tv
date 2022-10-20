655e6930e6b28d65b0a1d339994f67949da95a54
Adam Holtzman <adam.holtzman@gmail.com>
2022-10-20 08:58:05 -0400

    add skeleton for navigation (#40)
    * add skeleton for navigation
    
    * pr feednabck: lists && apply rebased styles
    
    * betetr window check
    
    * pr feedback: fix path check
    
    * clean up nav test code
    
    * clean up nav test code
    
    * clean up nav test code
    
    * clean up nav test code
    
    Co-authored-by: Owen Buckley <owen@thegreenhouse.io>

---
+++ src/components/nav/nav.js
@@ -0,0 +1,59 @@
+export default class Nav extends HTMLElement {
+  constructor() {
+    super();
+    this.navLinks = [
+      {
+        name: 'Home',
+        url: '/'
+      }, {
+        name: 'Events',
+        url: '/events'
+      }, {
+        name: 'Media',
+        url: '/media'
+      },
+      {
+        name: 'Merchendise',
+        url: '/merchendise'
+      },
+      {
+        name: 'The Crew',
+        url: '/the_crew'
+      }
+    ];
+  }
+
+  connectedCallback() {
+    const { navLinks } = this;
+
+    this.innerHTML = `
+      <nav>
+      <ul class="flex justify-center p-4 font-title text-2xl text-black-tt">
+        ${
+          navLinks.map((link, index) => {
+            const { name, url } = link;
+            const isLast = index + 1 === navLinks.length;
+            const path = typeof window === 'undefined' ? null : window.location.pathname;
+            const activePath = path === url;
+
+            return `
+              <li>
+                <a
+                  class="nav-link ${activePath ? 'text-orange-tt' : 'text-black-tt'} hover:text-gray-tt"
+                  href="${url}"
+                  title="Visit our ${name} page"
+                >
+                ${name}
+                </a>
+                <span style="margin: 0 ${isLast ? 0 : '1rem' };">${ isLast ? '' : '|'}</span>
+              </li>
+            `;
+          }).join('')
+        }
+        </ul>
+      </nav>
+    `;
+  }
+}
+
+customElements.define('tt-nav', Nav);
