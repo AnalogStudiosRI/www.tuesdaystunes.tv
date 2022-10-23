cd5cfddeea07ad9089ad03f8b2e0ea3994cdb64d
Owen Buckley <owen@thegreenhouse.io>
2022-10-23 19:24:33 -0400

    greeting message and minor tweaks

--- src/components/youtube-playlist/youtube-playlist.js
+++ src/components/youtube-playlist/youtube-playlist.js
@@ -13,7 +13,7 @@ export default class YouTubePlaylist extends HTMLElement {
           style="border-radius:12px"
           src="https://www.youtube.com/embed?listType=playlist&list=PLrY8SmqJ5XZ_UvVurEvGg8i10g-cxMudZ"
           width="100%"
-          height="380"
+          height="560"
           frameBorder="0"
           allowfullscreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,23 +18,43 @@ export default class Home extends HTMLElement {
       });
 
     this.innerHTML = `
-      <div id="container" class="bg-white rounded-lg drop-shadow-lg xs:p-2 mb-4 mt-2 m:p-12">
+      <div
+        class="bg-white rounded-lg drop-shadow-lg p-6 mb-4 mt-2"
+      >
 
-        <div id="greeting">
-          <div class="w-3/6 p-4 inline-block">
-            <img src="/assets/tuesdays-tunes.webp" class="w-2/5 block" style="margin: 0 auto"/>
+        <div id="greeting" class="grid grid-cols-6">
+          <div class="col-span-6 md:col-span-3">
+            <img src="/assets/tuesdays-tunes.webp" class="w-2/5 block m-0 ml-auto mr-auto"/>
 
             <tt-upcoming-events
               events='${JSON.stringify(events).replace(/'/g, '\\"')}'
+              class="block w-11/12 m-0 ml-auto mr-auto"
             ></tt-upcoming-events>
           </div>
 
-          <div class="inline">
-            <h1 class="font-extrabold">Welcome!  Tune into our live stream every other Tuesday.</h1>
-            <p>Join us every other Tuesday and enjoy selected songs sung by Dave Flamand himself.
-            We are looking forward to seeing you at our next live stream on Facebook.  Stay tuned
-            for any further details!</p>
-            <span>- Tunesy</span>
+          <div class="col-span-6 md:col-span-3 p-2">
+            <h1
+              class="font-extrabold uppercase text-3xl lg:text-5xl"
+              style="font-family:var(--font-secondary)"
+            >
+              Welcome!<br/> Tune into our live stream every other Tuesday.
+            </h1>
+
+            <p
+              class="mt-2 mb-2"
+              style="font-family:var(--font-secondary);"
+            >
+              Join us every other Tuesday and enjoy selected songs sung by Dave Flamand himself.
+              We are looking forward to seeing you at our next live stream on Facebook.  Stay tuned
+              for any further details!
+            </p>
+
+            <p
+              class="text-right mr-6"
+              style="font-family:var(--font-secondary);"
+            >
+              - Tunesy
+            </p>
           </div>
         </div>
 
