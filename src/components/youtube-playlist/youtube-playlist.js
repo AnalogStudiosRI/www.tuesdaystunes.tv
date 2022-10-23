141d92134c69d8029ab53f88a7e4bb05318ef4c9
Owen Buckley <owen@thegreenhouse.io>
2022-10-23 13:03:40 -0400

    update YouTube player implementation to an iframe

--- src/components/youtube-playlist/youtube-playlist.js
+++ src/components/youtube-playlist/youtube-playlist.js
@@ -1,47 +1,19 @@
-import imgPlaylist from './youtube-playlist-sample.json';
-import iframePlaylist from './youtube-playlist-sample2.json';
-
-// const fetchVideoPlaylist = async () => {
-//   const API_KEY = '';
-//   let playlists = await fetch(
-//     `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCxs5mxoDpmmR0hRbwsxU7Sg&key=${API_KEY}`
-//   )
-//     .then((result) => result.json())
-//     .then((ytPlaylists) => ytPlaylists.items);
-//   return playlists;
-// };
-
 export default class YouTubePlaylist extends HTMLElement {
   connectedCallback() {
-    // let ytplaylists = fetchVideoPlaylist();
-    let ytImgPlaylists = imgPlaylist.items;
-    let ytIframePlaylists = iframePlaylist.items;
-
     this.innerHTML = `
-            <h2 class='font-bold text-lg mb-2'>Youtube Videos with iframe</h2>
-            <div class='flex flex-wrap'>
-              ${ytIframePlaylists.map((playlist) => {
-      return `
-                  <div class='p-2 mb-4 mr-2'>
-                    ${playlist.player.embedHtml}
-                  </div>
-                `;
-    })}
-              </div>
-    
-              <h2 class='font-bold text-lg mb-2'>Youtube Img Playlist with Title and Date</h2>
-              <div class='flex flex-wrap'>
-                  ${ytImgPlaylists.map(playlist => {
-      return `
-                    <div class='border-2 border-yellow-500 text-center rounded-md w-80 p-2 mb-4 mr-2'>
-                    <img src='${playlist.snippet.thumbnails.medium.url}' alt='${playlist.snippet.title}' />
-                    <p>${playlist.snippet.title} - ${new Date(playlist.snippet.publishedAt).toLocaleDateString('en-UK')}</p>
-                  </div>
-                    `;
-    })}
-
-            </div>
-          `;
+      <div class="p-4 youtube-container">
+        <iframe
+          style="border-radius:12px"
+          src="https://www.youtube.com/embed?listType=playlist&list=PLrY8SmqJ5XZ_UvVurEvGg8i10g-cxMudZ"
+          width="100%"
+          height="380"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy">
+        </iframe>
+      </div>
+    `;
   }
 }
 
