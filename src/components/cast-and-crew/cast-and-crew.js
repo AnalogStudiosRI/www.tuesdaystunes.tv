0b89f1a79f02534de848d9f040b30aa05ac1c274
Owen Buckley <owen@thegreenhouse.io>
2022-10-23 13:16:16 -0400

    restore cast and crew files

--- src/components/cast-and-crew/cast-and-crew.js
+++ src/components/cast-and-crew/cast-and-crew.js
@@ -24,8 +24,6 @@ export default class CastAndCrew extends HTMLElement {
     const { profiles } = this;
 
     this.innerHTML = `
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div class="profiles">
         </hr>
         <h2 
@@ -35,26 +33,6 @@ export default class CastAndCrew extends HTMLElement {
           The Crew
         </h2>
         </hr>
-=======
-      <div>
-<<<<<<< HEAD
-        </br>
-        <h2 class="text-lg text-center">Cast and Crew</h2>
-        </br>
->>>>>>> 03dd8f9 (initial version of cast and crew section)
-=======
-=======
-      <div class="profiles">
->>>>>>> 08af55f (add test cases)
-        </hr>
-        <h2 
-          class="text-3xl text-center font-extrabold"
-          style="margin:0 auto 25px;width: 20%;font-family:var(--font-secondary); border-top: 2px solid rgba(107, 114, 128); border-bottom: 2px solid rgba(107, 114, 128);"
-        >
-          The Crew
-        </h2>
-        </hr>
->>>>>>> 6bd62ce (basic styling and theming of the header)
 
         <div class="grid md:grid-cols-4 grid-cols-2">
           ${
@@ -62,23 +40,9 @@ export default class CastAndCrew extends HTMLElement {
               const { avatar, name, title } = person;
 
               return `
-<<<<<<< HEAD
-<<<<<<< HEAD
                 <div class="text-center">
                   <img
                     class="block w-20 rounded-full text-center"
-=======
-                <div class="text-center"">
-=======
-                <div class="text-center">
->>>>>>> 08af55f (add test cases)
-                  <img
-<<<<<<< HEAD
-                    class="block w-10 rounded-full text-center"
->>>>>>> 03dd8f9 (initial version of cast and crew section)
-=======
-                    class="block w-20 rounded-full text-center"
->>>>>>> 6bd62ce (basic styling and theming of the header)
                     style="margin:0 auto"
                     src="${avatar}"
                     alt="Photo of ${name} (${title})"
@@ -86,15 +50,7 @@ export default class CastAndCrew extends HTMLElement {
                   <h3 class="font-bold text-center">
                     ${name}
                   </h3>
-<<<<<<< HEAD
-<<<<<<< HEAD
                   <h4 class="uppercase text-center text-gray-500">
-=======
-                  <h4 class="text-center text-gray-500">
->>>>>>> 03dd8f9 (initial version of cast and crew section)
-=======
-                  <h4 class="uppercase text-center text-gray-500">
->>>>>>> 6bd62ce (basic styling and theming of the header)
                     ${title}
                   </h3>
                 </div>
