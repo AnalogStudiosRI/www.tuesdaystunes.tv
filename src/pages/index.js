5f43fa7a44f999ef2ee9c2b574d3afb6f3b6e0b3
Owen Buckley <owenbuckley13@gmail.com>
2022-11-06 08:43:30 -0500

    restore previous above the fold layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,47 +21,44 @@ export default class Home extends HTMLElement {
     this.innerHTML = `
       <div
         class="bg-white rounded-lg drop-shadow-lg lg:p-8 m-auto mt-4 md:p-1"
-        style="max-width: 900px;"
       >
 
-        <div class="col-span-6 md:col-span-3 p-4 mt-4 mb-6">
-          <h1
-            class="font-extrabold uppercase text-3xl lg:text-5xl"
-            style="font-family:var(--font-secondary)"
-          >
-            Good Evening Folks!<br/>  Welcome to Tuesday&apos;s Tunes!!
-          </h1>
-
-          <p
-            class="mt-2 mb-2"
-            style="font-family:var(--font-secondary);"
-          >
-            Join us every other Tuesday and enjoy selected songs sung by Dave Flamand himself.
-            We are looking forward to seeing you at our next live stream on Facebook.  Stay tuned
-            for any further details!
-          </p>
-
-          <p
-            class="text-right mr-6"
-            style="font-family:var(--font-secondary);"
-          >
-            - Tunesy
-          </p>
-        </div>
-
-        <div id="greeting" class="p-8 mb-6">
+        <div id="greeting" class="grid grid-cols-6">
           <div class="col-span-6 md:col-span-3">
             <img
               src="/assets/tunesy.webp"
               alt="Picture of Tunesy"
-              class="w-2/5 block m-0 mb-12 ml-auto mr-auto"
+              class="w-2/5 block m-0 ml-auto mr-auto"
               height="300"
               width="285"
             />
             <tt-upcoming-events
               events='${JSON.stringify(events).replace(/'/g, '\\"')}'
-              class="block m-0 ml-auto mr-auto"
+              class="block w-11/12 m-0 ml-auto mr-auto"
             ></tt-upcoming-events>
+          </div>
+          <div class="col-span-6 md:col-span-3 p-2">
+            <h1
+              class="font-extrabold uppercase text-3xl lg:text-5xl"
+              style="font-family:var(--font-secondary)"
+            >
+              Good Evening Folks!<br/>
+              Welcome to Tuesday&apos;s Tunes!!
+            </h1>
+            <p
+              class="mt-2 mb-2"
+              style="font-family:var(--font-secondary);"
+            >
+              Join us every other Tuesday and enjoy selected songs sung by Dave Flamand himself.
+              We are looking forward to seeing you at our next live stream on Facebook.  Stay tuned
+              for any further details!
+            </p>
+            <p
+              class="text-right mr-6"
+              style="font-family:var(--font-secondary);"
+            >
+              &#8211; Tunesy
+            </p>
           </div>
         </div>
 
