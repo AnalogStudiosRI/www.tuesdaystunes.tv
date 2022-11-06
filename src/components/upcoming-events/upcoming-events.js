7842daf53194441335b0182478171d8ad472fdd5
Owen Buckley <owen@thegreenhouse.io>
2022-11-06 10:35:01 -0500

    revert formatting

--- src/components/upcoming-events/upcoming-events.js
+++ src/components/upcoming-events/upcoming-events.js
@@ -32,7 +32,9 @@ export default class UpcomingEvents extends HTMLElement {
         return startTime >= now.getTime() || isInCurrentMonth;
       })
       .sort((a, b) => a.startTime < b.startTime ? -1 : 1); // sort newest to latest
-    const noEvents = events.length === 0 ? '<h2 class="text-center">No Upcoming Events</h2>' : '';
+    const noEvents = events.length === 0
+      ? '<h2 class="text-center">No Upcoming Events</h2>'
+      : '';
 
     // group events by month
     events.forEach((event) => {
@@ -50,56 +52,56 @@ export default class UpcomingEvents extends HTMLElement {
     /* eslint-disable indent */
     this.innerHTML = `
       <div class="upcoming-events">
-        <h2 
+        <h2
           class="text-3xl text-center font-extrabold"
           style="font-family:var(--font-secondary);border-bottom: 2px solid rgba(107, 114, 128);"
         >
           Episode Schedule
         </h2>
         ${noEvents}
-  
+
         ${Object.keys(eventsByMonth).map((month) => {
-            return `
-              <div class="mb-6">
-                <h3
-                  style="background-color:var(--color-secondary);color:var(--color-white);font-family:var(--font-secondary)"
-                  class="text-center p-2 mb-4 text-3xl font-bold"
-                >
-                  ${month}
-                </h2>
+          return `
+            <div class="mb-6">
+              <h3
+                style="background-color:var(--color-secondary);color:var(--color-white);font-family:var(--font-secondary)"
+                class="text-center p-2 mb-4 text-3xl font-bold"
+              >
+                ${month}
+              </h2>
 
-                ${eventsByMonth[month].map((event) => {
-                    const { startTime, title } = event;
-                    const time = new Date(startTime);
-                    const hours = time.getHours();
-                    const formattedTitle = title.replace(/"/g, "'"); // TODO https://github.com/AnalogStudiosRI/www.tuesdaystunes.tv/issues/47
-                    const date = time.getDate();
-                    const hour = hours > 12 ? hours - 12 : hours; // here we assume an 8pm (e.g. afternoon) start time
+              ${eventsByMonth[month].map((event) => {
+                const { startTime, title } = event;
+                const time = new Date(startTime);
+                const hours = time.getHours();
+                const formattedTitle = title.replace(/"/g, "'"); // TODO https://github.com/AnalogStudiosRI/www.tuesdaystunes.tv/issues/47
+                const date = time.getDate();
+                const hour = hours > 12 ? hours - 12 : hours; // here we assume an 8pm (e.g. afternoon) start time
 
-                    return `
-                    <div>
-                      <h4
-                        style="color:var(--color-white); margin: .5rem auto;"
-                        class="w-11/12 sm:w-10/12 flex"
+                return `
+                  <div>
+                    <h4
+                      style="color:var(--color-white); margin: .5rem auto;"
+                      class="w-11/12 sm:w-10/12 flex"
+                    >
+                      <span
+                        class="inline-block w-8 h-[1.6rem] text-center mr-2"
+                        style="background-color:var(--color-accent);"
                       >
-                        <span
-                          class="inline-block w-8 h-[1.6rem] text-center mr-2"
-                          style="background-color:var(--color-accent);"
-                        >
-                          ${date}
-                        </span>
-                        <span
-                          style="color:var(--color-secondary);"
-                        >
-                          ${formattedTitle} @ ${hour}pm
-                        </span>
-                      </h4>
-                    </div>
-                  `;
-                  }).join('')}
-              </div>
-            `;
-          }).join('')}      
+                        ${date}
+                      </span>
+                      <span
+                        style="color:var(--color-secondary);"
+                      >
+                        ${formattedTitle} @ ${hour}pm
+                      </span>
+                    </h4>
+                  </div>
+                `;
+              }).join('')}
+            </div>
+          `;
+        }).join('')}
       </div>
     `;
   }
