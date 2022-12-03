c2a5cf2f13a200fdc4ab22b0307118c7698dfd24
Owen Buckley <owen@thegreenhouse.io>
2022-12-03 15:49:13 -0500

    rewrite assets path for storybook github pages builds (#95)
    * rewrite assets path for storybook github pages builds
    
    * additional comments
    
    * fix lint

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   staticDirs: ['../src'],
   stories: [
@@ -9,5 +11,24 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-postcss'
   ],
-  framework: '@storybook/web-components'
+  framework: '@storybook/web-components',
+  // set publicPath (e.g. `<base href="..." />`) for GitHub Pages
+  // https://github.com/storybookjs/storybook/issues/12444#issuecomment-1179671255
+  // Note: Used string-replace-loader@^2.x for webpack v4 compat to support current Storybook
+  webpackFinal: async (config) => {
+    if (process.env.GH_PAGES) {
+      config.module.rules.push({
+        test: /.js$/,
+        include: [
+          path.resolve(__dirname, '../src/components')
+        ],
+        loader: 'string-replace-loader',
+        options: {
+          search: /\/assets\//g,
+          replace: '/www.tuesdaystunes.tv/assets/'
+        }
+      });
+    }
+    return config;
+  }
 };
