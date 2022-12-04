d725d2ea0ffc91390cd27fc7c8af5c21865a0cef
Owen Buckley <owen@thegreenhouse.io>
2022-12-04 18:56:22 -0500

    display an indicator besides the next closest upcoming event (#99)
    * display an indicator besides the next closest upcoming event
    
    * fix lint

--- src/components/upcoming-events/mock-events.js
+++ src/components/upcoming-events/mock-events.js
@@ -1,4 +1,5 @@
 const SINGLE_EVENT = [{
+  id: 1,
   title: 'Tuesday\'s Tunes Season 3 - Teaser Trailer!',
   startTime: Date.now() + 300000,
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.'
@@ -6,6 +7,7 @@ const SINGLE_EVENT = [{
 
 // includes testing for multiple months, events list "overflow", and out of order events
 const MULTIPLE_EVENTS = [{
+  id: 3,
   title: 'Tuesday\'s Tunes Season 3 - Episode 1',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 15),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
@@ -14,16 +16,19 @@ const MULTIPLE_EVENTS = [{
   ...SINGLE_EVENT[0],
   link: 'http://www.facebook.com/'
 }, {
+  id: 4,
   title: 'Tuesday\'s Tunes Season 3 - Episode 2',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 30),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
   link: 'http://www.facebook.com/'
 }, {
+  id: 2,
   title: 'Tuesday\'s Tunes Season 3 - Premier',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 5),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
   link: 'http://www.facebook.com/'
 }, {
+  id: 5,
   title: 'Tuesday\'s Tunes Season 3 - Episode 3',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 45),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
