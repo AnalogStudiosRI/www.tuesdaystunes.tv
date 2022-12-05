9fc5be0c424d34146c24127cdba1a9f8f80ac734
Owen Buckley <owen@thegreenhouse.io>
2022-12-04 19:39:47 -0500

    render YouTube playlist from fetched API data

--- src/components/youtube-playlist/youtube-playlist.js
+++ src/components/youtube-playlist/youtube-playlist.js
@@ -1,5 +1,13 @@
+import fetch from 'node-fetch';
+
 export default class YouTubePlaylist extends HTMLElement {
-  connectedCallback() {
+  async connectedCallback() {
+    // https://developers.google.com/youtube/v3/docs/playlistItems
+    const playlistItems = (await fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?key=${process.env.API_KEY_YOUTUBE}&playlistId=PLrY8SmqJ5XZ_UvVurEvGg8i10g-cxMudZ&maxResults=50&part=snippet`) // eslint-disable-line max-len
+      .then(resp => resp.json()))
+      .items.map(item => item.snippet)
+      .reverse();
+    
     this.innerHTML = `
       <h2
         class="text-3xl text-center font-extrabold"
@@ -8,19 +16,25 @@ export default class YouTubePlaylist extends HTMLElement {
         Past Episodes
       </h2>
 
-      <div class="p-4 youtube-container">
-        <iframe
-          style="border-radius:12px"
-          src="https://www.youtube.com/embed?listType=playlist&list=PLrY8SmqJ5XZ_UvVurEvGg8i10g-cxMudZ"
-          width="100%"
-          height="500px"
-          title="Spotify Playlist"
-          frameBorder="0"
-          allowfullscreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy">
-        </iframe>
-      </div>
+      ${
+        playlistItems.map((item) => {
+          return `
+            <div class="p-4 youtube-container">
+              <iframe
+                style="border-radius:12px"
+                src="https://www.youtube.com/embed/${item.resourceId.videoId}"
+                width="100%"
+                height="500px"
+                title="Spotify Playlist"
+                frameBorder="0"
+                allowfullscreen=""
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                loading="lazy">
+              </iframe>
+            </div>
+          `;
+        }).join('')
+      }
     `;
   }
 }
