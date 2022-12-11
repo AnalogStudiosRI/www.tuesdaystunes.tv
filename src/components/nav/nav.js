cb6c95e8546bcdd88e7f817a7dfd481e00e77219
Owen Buckley <owen@thegreenhouse.io>
2022-12-11 13:04:41 -0500

    basic support for isomorhpic rendering

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -15,33 +15,43 @@ export default class Nav extends HTMLElement {
   connectedCallback() {
     const { navLinks } = this;
 
-    this.innerHTML = `
-      <nav>
-      <ul class="flex justify-center p-4 font-title text-2xl text-black-tt">
-        ${
-          navLinks.map((link, index) => {
-            const { name, url } = link;
-            const isLast = index + 1 === navLinks.length;
-            const path = typeof globalThis.location === 'undefined' ? null : globalThis.location.pathname;
-            const activePath = path === url;
+    if (this.innerHTML !== '') {
+      Array.from(this.querySelectorAll('a')).forEach(link => {
+        const path = globalThis.location.pathname;
 
-            return `
-              <li>
-                <a
-                  class="nav-link ${activePath ? 'text-orange-tt' : 'text-black-tt'} hover:text-gray-tt"
-                  href="${url}"
-                  title="Visit our ${name} page"
-                >
-                ${name}
-                </a>
-                <span style="margin: 0 ${isLast ? 0 : '1rem' };">${ isLast ? '' : '|'}</span>
-              </li>
-            `;
-          }).join('')
+        if (link.getAttribute('href').replace(window.location.origin, '') === path) {
+          link.style.color = 'var(--color-accent)';
         }
-        </ul>
-      </nav>
-    `;
+      });
+    } else {
+      this.innerHTML = `
+        <nav>
+        <ul class="flex justify-center p-4 font-title text-2xl text-black-tt">
+          ${
+            navLinks.map((link, index) => {
+              const { name, url } = link;
+              const isLast = index + 1 === navLinks.length;
+              const path = typeof globalThis.location === 'undefined' ? null : globalThis.location.pathname;
+              const activePath = path === url;
+
+              return `
+                <li>
+                  <a
+                    class="nav-link ${activePath ? 'text-orange-tt' : 'text-black-tt'} hover:text-gray-tt"
+                    href="${url}"
+                    title="Visit our ${name} page"
+                  >
+                  ${name}
+                  </a>
+                  <span style="margin: 0 ${isLast ? 0 : '1rem' };">${ isLast ? '' : '|'}</span>
+                </li>
+              `;
+            }).join('')
+          }
+          </ul>
+        </nav>
+      `;
+    }
   }
 }
 
