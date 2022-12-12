494071673e7cd3929214e3214b5f0dd29a52815d
Braedon Creal <59781654+bjcreal@users.noreply.github.com>
2022-12-11 23:49:45 -0500

    cleaned up unnecessary changes

--- src/components/upcoming-events/mock-events.js
+++ src/components/upcoming-events/mock-events.js
@@ -2,7 +2,7 @@ const SINGLE_EVENT = [{
   id: 1,
   title: 'Tuesday\'s Tunes Season 3 - Teaser Trailer!',
   startTime: Date.now() + 300000,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.'
 }];
 
 // includes testing for multiple months, events list "overflow", and out of order events
@@ -11,7 +11,7 @@ const MULTIPLE_EVENTS = [{
   title: 'Tuesday\'s Tunes Season 3 - Episode 1',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 15),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
-  link: 'http://www.facebook.com/',
+  link: 'http://www.facebook.com/'
 }, {
   ...SINGLE_EVENT[0],
   link: 'http://www.facebook.com/'
@@ -20,19 +20,19 @@ const MULTIPLE_EVENTS = [{
   title: 'Tuesday\'s Tunes Season 3 - Episode 2',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 30),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
-  link: 'http://www.facebook.com/',
+  link: 'http://www.facebook.com/'
 }, {
   id: 2,
   title: 'Tuesday\'s Tunes Season 3 - Premier',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 5),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
-  link: 'http://www.facebook.com/',
+  link: 'http://www.facebook.com/'
 }, {
   id: 5,
   title: 'Tuesday\'s Tunes Season 3 - Episode 3',
   startTime: SINGLE_EVENT[0].startTime + (86400000 * 45),
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elite.',
-  link: 'http://www.facebook.com/',
+  link: 'http://www.facebook.com/'
 }];
 
 const NO_EVENTS = [];
