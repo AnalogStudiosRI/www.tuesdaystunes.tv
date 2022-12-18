ff5227c10a238ef2de1c5cb103cd24673c261977
thescientist13 <thescientist13@users.noreply.github.com>
2022-12-18 20:42:05 +0000

    deploy: aa077ff55ecbfd830311980537c726549422f8f9

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -6,19 +6,8 @@ export default class Nav extends HTMLElement {
         name: 'Home',
         url: '/'
       }, {
-        name: 'Events',
-        url: '/events'
-      }, {
-        name: 'Media',
-        url: '/media'
-      },
-      {
-        name: 'Merchendise',
-        url: '/merchendise'
-      },
-      {
-        name: 'The Crew',
-        url: '/the_crew'
+        name: 'Contact',
+        url: '/contact/'
       }
     ];
   }
@@ -26,33 +15,43 @@ export default class Nav extends HTMLElement {
   connectedCallback() {
     const { navLinks } = this;
 
-    this.innerHTML = `
-      <nav>
-      <ul class="flex justify-center p-4 font-title text-2xl text-black-tt">
-        ${
-          navLinks.map((link, index) => {
-            const { name, url } = link;
-            const isLast = index + 1 === navLinks.length;
-            const path = typeof window === 'undefined' ? null : window.location.pathname;
-            const activePath = path === url;
+    if (this.innerHTML !== '') {
+      // if already server-rendered, all we need to do is highlight the active link
+      Array.from(this.querySelectorAll('a')).forEach(link => {
+        const path = globalThis.location.pathname;
+        const isCurrentLink = link.getAttribute('href').replace(window.location.origin, '') === path;
 
-            return `
-              <li>
-                <a
-                  class="nav-link ${activePath ? 'text-orange-tt' : 'text-black-tt'} hover:text-gray-tt"
-                  href="${url}"
-                  title="Visit our ${name} page"
-                >
-                ${name}
-                </a>
-                <span style="margin: 0 ${isLast ? 0 : '1rem' };">${ isLast ? '' : '|'}</span>
-              </li>
-            `;
-          }).join('')
-        }
-        </ul>
-      </nav>
-    `;
+        link.style.color = isCurrentLink ? 'var(--color-accent)' : 'var(--color-black)';
+      });
+    } else {
+      this.innerHTML = `
+        <nav>
+        <ul class="flex justify-center p-4 font-title text-2xl text-black-tt">
+          ${
+            navLinks.map((link, index) => {
+              const { name, url } = link;
+              const isLast = index + 1 === navLinks.length;
+              const path = typeof globalThis.location === 'undefined' ? null : globalThis.location.pathname;
+              const activePath = path === url;
+
+              return `
+                <li>
+                  <a
+                    class="nav-link ${activePath ? 'text-orange-tt' : 'text-black-tt'} hover:text-gray-tt"
+                    href="${url}"
+                    title="Visit our ${name} page"
+                  >
+                  ${name}
+                  </a>
+                  <span style="margin: 0 ${isLast ? 0 : '1rem' };">${ isLast ? '' : '|'}</span>
+                </li>
+              `;
+            }).join('')
+          }
+          </ul>
+        </nav>
+      `;
+    }
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -16,56 +16,50 @@ export default class Home extends HTMLElement {
       });
 
     this.innerHTML = `
-      <div
-        class="bg-white rounded-lg drop-shadow-lg lg:p-8 m-auto mt-4 md:p-1"
-      >
-
-        <div id="greeting" class="grid grid-cols-6">
-          <div class="col-span-6 md:col-span-3">
-            <img
-              src="/assets/tunesy.webp"
-              alt="Picture of Tunesy"
-              class="w-2/5 block ml-auto mr-auto mt-4 lg:mt-0"
-              height="300"
-              width="285"
-            />
-            <tt-upcoming-events
-              events='${JSON.stringify(events).replace(/'/g, '\\"')}'
-              class="block w-11/12 m-0 ml-auto mr-auto"
-            ></tt-upcoming-events>
-          </div>
-          <div class="col-span-6 md:col-span-3 p-2">
-            <h1
-              class="font-extrabold uppercase text-3xl lg:text-5xl"
-              style="font-family:var(--font-secondary)"
-            >
-              Good Evening Folks!<br/>
-              Welcome to Tuesday&apos;s Tunes!!
-            </h1>
-            <p
-              class="mt-2 mb-2"
-              style="font-family:var(--font-secondary);"
-            >
-              Join us every other Tuesday and enjoy selected songs sung by Dave Flamand himself.
-              We are looking forward to seeing you at our next live stream on Facebook.  Stay tuned
-              for any further details!
-            </p>
-            <p
-              class="text-right mr-6"
-              style="font-family:var(--font-secondary);"
-            >
-              &#8211; Tunesy
-            </p>
-          </div>
+      <div id="greeting" class="grid grid-cols-6">
+        <div class="col-span-6 md:col-span-3">
+          <img
+            src="/assets/tunesy.webp"
+            alt="Picture of Tunesy"
+            class="w-2/5 block ml-auto mr-auto mt-4 lg:mt-0"
+            height="300"
+            width="285"
+          />
+          <tt-upcoming-events
+            events='${JSON.stringify(events).replace(/'/g, '\\"')}'
+            class="block w-11/12 m-0 ml-auto mr-auto"
+          ></tt-upcoming-events>
         </div>
-
-        <tt-youtube-playlist class="block mb-12"></tt-youtube-playlist>
-
-        <tt-spotify-playlist class="block mb-12"></tt-spotify-playlist>
-
-        <tt-cast-crew class="block mb-6"></tt-cast-crew>
-
+        <div class="col-span-6 md:col-span-3 p-2">
+          <h1
+            class="font-extrabold uppercase text-3xl lg:text-5xl"
+            style="font-family:var(--font-secondary)"
+          >
+            Good Evening Folks!<br/>
+            Welcome to Tuesday&apos;s Tunes!!
+          </h1>
+          <p
+            class="mt-2 mb-2"
+            style="font-family:var(--font-secondary);"
+          >
+            Join us every other Tuesday and enjoy selected songs sung by Dave Flamand himself.
+            We are looking forward to seeing you at our next live stream on Facebook.  Stay tuned
+            for any further details!
+          </p>
+          <p
+            class="text-right mr-6"
+            style="font-family:var(--font-secondary);"
+          >
+            &#8211; Tunesy
+          </p>
+        </div>
       </div>
+
+      <tt-youtube-playlist class="block mb-12"></tt-youtube-playlist>
+
+      <tt-spotify-playlist class="block mb-12"></tt-spotify-playlist>
+
+      <tt-cast-crew class="block mb-6"></tt-cast-crew>
     `;
   }
 }
