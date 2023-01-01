5e40257b5d3bc31ac13299308f2b13830d1ab60b
Owen Buckley <owen@thegreenhouse.io>
2023-01-01 18:47:01 -0500

    test adapter

---
+++ src/api/greeting-adapter.js
@@ -0,0 +1,16 @@
+import { handler as greeting } from './greeting.js';
+
+// https://docs.netlify.com/functions/build/?fn-language=js#code-your-function-2
+export default async function handler (event, context) {
+  console.log({ event, context });
+  const request = new Request(event.url);
+  console.log(request);
+  const response = greeting(request);
+  console.log(response);
+
+  // TODO can we just return response.body, or response instead?
+  return {
+    statusCode: response.status,
+    body: await response.text()
+  };
+};
