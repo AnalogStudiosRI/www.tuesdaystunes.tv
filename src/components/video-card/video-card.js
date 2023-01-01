a92acd1c0a9f049975cf77f6ef14b40392196b79
Owen Buckley <owen@thegreenhouse.io>
2023-01-01 17:53:22 -0500

    add basic interactivity example

--- src/components/video-card/video-card.js
+++ src/components/video-card/video-card.js
@@ -1,4 +1,9 @@
 export default class VideoCard extends HTMLElement {
+
+  selectVideo() {
+    alert(`selected video to play is => ${this.getAttribute('title')}!`);
+  };
+
   connectedCallback() {
     const thumbnail = this.getAttribute('thumbnail');
     const title = this.getAttribute('title');
@@ -7,7 +12,7 @@ export default class VideoCard extends HTMLElement {
       <div class="p-4 youtube-container">
         <h1>${title}</h1> 
         <img src="${thumbnail}" loading="lazy">
-        <button>View Broadcast</button>
+        <button onclick="this.parentNode.parentNode.selectVideo()">View Broadcast</button>
         <hr/>
       </div>
     `;
