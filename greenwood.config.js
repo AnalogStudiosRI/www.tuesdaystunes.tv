4cf446a75b7610df644aae09970914da1cacdea1
Owen Buckley <owen@thegreenhouse.io>
2023-01-02 11:15:48 -0500

    upgrade greenwood v0.28.0-alpha.0

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,12 +1,10 @@
 import { greenwoodPluginPostCss } from '@greenwood/plugin-postcss';
-import { greenwoodApiRoutesPlugin } from './api-routes-plugin.js';
 
 export default {
   prerender: true,
   plugins: [
     greenwoodPluginPostCss({
       extendConfig: true
-    }),
-    greenwoodApiRoutesPlugin()
+    })
   ]
 };
