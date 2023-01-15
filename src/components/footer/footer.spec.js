8e856f33c8e1822d7394f2e583605595d19cb931
Owen Buckley <owen@thegreenhouse.io>
2023-01-15 14:14:30 -0500

    update and add specs

--- src/components/footer/footer.spec.js
+++ src/components/footer/footer.spec.js
@@ -19,83 +19,9 @@ describe('Components/Footer', () => {
     });
   });
 
-  describe('Social Links', () => {
-    let links = [];
-    let logos = [];
-
-    before(async () => {
-      links = footer.querySelectorAll('footer a.social-link');
-      logos = document.querySelectorAll('footer a.social-link img');
-    });
-
-    it('should have the expected social links and logos in the footer', () => {
-      expect(links.length).to.equal(5);
-      expect(logos.length).to.equal(5);
-    });
-
-    it('should have the expected Facebook link and logo in the footer', () => {
-      const name = 'Facebook';
-      const facebookLink = Array.from(links).filter(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
-      const facebookLogo = Array.from(logos).filter(logo => logo.getAttribute('src').indexOf(`${name.toLowerCase()}.svg`) >= 0);
-
-      expect(facebookLink.length).to.equal(1);
-      expect(facebookLink[0].getAttribute('title')).to.equal(`Visit our ${name} page`);
-
-      expect(facebookLogo.length).to.equal(1);
-      expect(facebookLogo[0].getAttribute('alt')).to.equal(`${name} Logo`);
-    });
-
-    it('should have the expected YouTube link and logo in the footer', () => {
-      const name = 'YouTube';
-      const youtubeLink = Array.from(links).filter(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
-      const youtubeLogo = Array.from(logos).filter(logo => logo.getAttribute('src').indexOf(`${name.toLowerCase()}.svg`) >= 0);
-
-      expect(youtubeLink.length).to.equal(1);
-      expect(youtubeLink[0].getAttribute('title')).to.equal(`Visit our ${name} page`);
-
-      expect(youtubeLogo.length).to.equal(1);
-      expect(youtubeLogo[0].getAttribute('alt')).to.equal(`${name} Logo`);
-    });
-
-    it('should have the expected Instagram link and logo in the footer', () => {
-      const name = 'Instagram';
-      const instagramLink = Array.from(links).filter(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
-      const youtubeLogo = Array.from(logos).filter(logo => logo.getAttribute('src').indexOf(`${name.toLowerCase()}.svg`) >= 0);
-
-      expect(instagramLink.length).to.equal(1);
-      expect(instagramLink[0].getAttribute('title')).to.equal(`Visit our ${name} page`);
-
-      expect(youtubeLogo.length).to.equal(1);
-      expect(youtubeLogo[0].getAttribute('alt')).to.equal(`${name} Logo`);
-    });
-
-    it('should have the expected TikTok link and logo in the footer', () => {
-      const name = 'TikTok';
-      const tiktokLink = Array.from(links).filter(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
-      const tiktokLogo = Array.from(logos).filter(logo => logo.getAttribute('src').indexOf(`${name.toLowerCase()}.svg`) >= 0);
-
-      expect(tiktokLink.length).to.equal(1);
-      expect(tiktokLink[0].getAttribute('title')).to.equal(`Visit our ${name} page`);
-
-      expect(tiktokLogo.length).to.equal(1);
-      expect(tiktokLogo[0].getAttribute('alt')).to.equal(`${name} Logo`);
-    });
-
-    it('should have the expected Spotify link and logo in the footer', () => {
-      const name = 'Spotify';
-      const spotifyLink = Array.from(links).filter(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
-      const spotifyLogo = Array.from(logos).filter(logo => logo.getAttribute('src').indexOf(`${name.toLowerCase()}.svg`) >= 0);
-
-      expect(spotifyLink.length).to.equal(1);
-      expect(spotifyLink[0].getAttribute('title')).to.equal(`Visit our ${name} page`);
-
-      expect(spotifyLogo.length).to.equal(1);
-      expect(spotifyLogo[0].getAttribute('alt')).to.equal(`${name} Logo`);
-    });
-
-    after(async () => {
-      links = [];
-      logos = [];
+  describe('Social Links', () => {    
+    it('should have the <tt-social-links> component in the footer', () => {
+      expect(footer.querySelectorAll('tt-social-links').length).equal(1);
     });
   });
 
---
+++ src/components/social-links/social-links.spec.js
@@ -0,0 +1,83 @@
+import { expect } from '@esm-bundle/chai';
+import './social-links.js';
+
+describe('Components/Social Links', () => {
+  let socialLinks;
+
+  before(async () => {
+    socialLinks = document.createElement('tt-social-links');
+
+    document.body.appendChild(socialLinks);
+
+    await socialLinks.updateComplete;
+  });
+
+  describe('Default Behavior', () => {    
+    it('should not be null', () => {
+      expect(socialLinks).not.equal(undefined);
+      expect(socialLinks.querySelectorAll('div.social-links').length).equal(1);
+    });
+  });
+
+  describe('Social Links', () => {
+    let links = [];
+    let logos = [];
+
+    before(async () => {
+      links = socialLinks.querySelectorAll('div a.social-link');
+      logos = socialLinks.querySelectorAll('div a.social-link svg');
+    });
+    
+    it('should have the expected social links and logos in the container', () => {
+      expect(links.length).to.equal(3);
+      expect(logos.length).to.equal(3);
+    });
+
+    it('should have the expected Facebook link and logo in the footer', () => {
+      const name = 'Facebook';
+      const facebookLink = Array.from(links).find(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
+      const facebookLogo = facebookLink.querySelectorAll('svg');
+
+      expect(facebookLink).to.not.equal(undefined);
+      expect(facebookLink.getAttribute('title')).to.equal(`Visit our ${name} page`);
+
+      expect(facebookLogo.length).to.equal(1);
+      expect(facebookLogo[0].getAttribute('width')).to.equal('100%');
+    });
+
+    it('should have the expected YouTube link and logo in the footer', () => {
+      const name = 'YouTube';
+      const youtubeLink = Array.from(links).find(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
+      const youtubeLogo = youtubeLink.querySelectorAll('svg');
+
+      expect(youtubeLink).to.not.equal(undefined);
+      expect(youtubeLink.getAttribute('title')).to.equal(`Visit our ${name} page`);
+
+      expect(youtubeLogo.length).to.equal(1);
+      expect(youtubeLogo[0].getAttribute('width')).to.equal('100%');
+    });
+
+    it('should have the expected Spotify link and logo in the footer', () => {
+      const name = 'Spotify';
+      const spotifyLink = Array.from(links).find(link => link.getAttribute('href').indexOf(`${name.toLowerCase()}.com`) >= 0);
+      const spotifyLogo = spotifyLink.querySelectorAll('svg');
+
+      expect(spotifyLink).to.not.equal(undefined);
+      expect(spotifyLink.getAttribute('title')).to.equal(`Visit our ${name} page`);
+
+      expect(spotifyLogo.length).to.equal(1);
+      expect(spotifyLogo[0].getAttribute('width')).to.equal('100%');
+    });
+
+    after(async () => {
+      links = [];
+      logos = [];
+    });
+  });
+
+  after(() => {
+    socialLinks.remove();
+    socialLinks = null;
+  });
+
+});
