ea07a5ba729012af06869560465414cb891a5fcb
Owen Buckley <owen@thegreenhouse.io>
2023-01-15 11:49:32 -0500

    initial custom SSR + SVG implementation

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,10 +1,12 @@
 import { greenwoodPluginPostCss } from '@greenwood/plugin-postcss';
+import { greenwoodPluginImportSvg } from './plugin-import-svg.js';
 
 export default {
   prerender: true,
   plugins: [
     greenwoodPluginPostCss({
       extendConfig: true
-    })
+    }),
+    greenwoodPluginImportSvg()
   ]
 };
