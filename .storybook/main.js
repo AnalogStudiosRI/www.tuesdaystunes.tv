4a3c15a009fb1140ab44848990aaf1e82167c129
Owen Buckley <owen@thegreenhouse.io>
2023-04-15 10:52:36 -0400

    Chore/upgrade storybook7 (#117)
    * upgrade to storybook7
    
    * clean up deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,28 @@
-const path = require('path');
+import path from 'path';
 
 module.exports = {
   staticDirs: ['../src'],
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-postcss'
-  ],
-  framework: '@storybook/web-components',
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
+    }
+  }],
+  framework: {
+    name: '@storybook/web-components-webpack5',
+    options: {}
+  },
   // set publicPath (e.g. `<base href="..." />`) for GitHub Pages
   // https://github.com/storybookjs/storybook/issues/12444#issuecomment-1179671255
   // Note: Used string-replace-loader@^2.x for webpack v4 compat to support current Storybook
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     if (process.env.GH_PAGES) {
       config.module.rules.push({
         test: /.js$/,
-        include: [
-          path.resolve(__dirname, '../src/components')
-        ],
+        include: [path.resolve(__dirname, '../src/components')],
         loader: 'string-replace-loader',
         options: {
           search: /\/assets\//g,
@@ -30,5 +31,8 @@ module.exports = {
       });
     }
     return config;
+  },
+  docs: {
+    autodocs: true
   }
 };
