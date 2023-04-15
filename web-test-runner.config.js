52cd9b6124403604ad57a4a5b0ce8a6bc15fde7d
Owen Buckley <owen@thegreenhouse.io>
2023-04-15 11:02:09 -0400

    upgrade greenwood and GitHub Actions for node 18 (#110)
    * upgrade greenwood and GitHub Actions for node 18
    
    * update v0.28.0-alpha.1
    
    * upgrade actions/setup-node to v3
    
    * upgrade greenwood alpha.2
    
    * upgrade greenwood alpha.3
    
    * upgrade alpha.5
    
    * upgrade greenwood v0.28.0

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,10 +1,6 @@
 import { defaultReporter } from '@web/test-runner';
-// import { greenwoodPluginImportCss } from '@greenwood/plugin-import-css/src/index.js';
 import { junitReporter } from '@web/test-runner-junit-reporter';
 import path from 'path';
-
-// create a direct instance of ImportCssResource
-// const importCssResource = greenwoodPluginImportCss()[0].provider({});
 
 export default {
   files: './src/**/*.spec.js',
@@ -19,25 +15,6 @@ export default {
   coverageConfig: {
     reportDir: './reports'
   },
-  // plugins: [{
-  //   name: 'import-css',
-  //   async transform(context) {
-  //     const { url } = context.request;
-  //     const shouldIntercept = await importCssResource.shouldIntercept(url, context.body, { request: context.headers });
-      
-  //     if (shouldIntercept) {
-  //       const cssResource = await importCssResource.intercept(url, context.body);
-  //       const { body, contentType } = cssResource;
-
-  //       return {
-  //         body,
-  //         headers: {
-  //           'content-type': contentType
-  //         }
-  //       };
-  //     }
-  //   }
-  // }],
   middleware: [
     function rewriteIndex(context, next) {
       const { url } = context.request;
